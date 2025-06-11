06dfa7b09d0c22e4ed271a0c6797e8cba3cdda48
JPPOKE <jppokedev@gmail.com>
2025-06-11 17:50:06 -0300

    Projeto Criado

---
+++ src/main/java/com/jppokedev/modulomongo/ModulomongoApplication.java
@@ -0,0 +1,13 @@
+package com.jppokedev.modulomongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulomongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModulomongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jppokedev/modulomongo/ModulomongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jppokedev.modulomongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulomongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
