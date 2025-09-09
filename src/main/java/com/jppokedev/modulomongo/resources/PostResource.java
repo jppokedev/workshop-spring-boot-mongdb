f7bf95fba695b199585558191e5d09a0a99fd58b
jppokedev <jppokedev@gmail.com>
2025-09-09 13:28:54 -0300

    Consulta simples com query methods

--- src/main/java/com/jppokedev/modulomongo/resources/PostResource.java
+++ src/main/java/com/jppokedev/modulomongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.jppokedev.modulomongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jppokedev.modulomongo.domain.Post;
+import com.jppokedev.modulomongo.resources.util.URL;
 import com.jppokedev.modulomongo.services.PostService;
 
 
@@ -24,4 +28,12 @@ public class PostResource {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@GetMapping(value= "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text){
+		text = URL.decodeParam(text);		
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
 }
